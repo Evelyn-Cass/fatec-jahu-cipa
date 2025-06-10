25c4a9eb875e713145465b1c6e6baa80e107f8f7
Evelyn Cassinotte <e.cass03@gmail.com>
2025-06-10 12:31:00 -0300

    Modified: Style on Legislation and Studies Tables

--- CipaFatecJahu/obj/Debug/net9.0/CipaFatecJahu.AssemblyInfo.cs
+++ CipaFatecJahu/obj/Debug/net9.0/CipaFatecJahu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CipaFatecJahu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe7ff87cb28c066d2509f4385aa1a59773938436")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1f14fbef2d83be4b208baa513984667c6488bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CipaFatecJahu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CipaFatecJahu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
