64cb9cc5afdc205658f114c776b2feeed4a61373
Evelyn Cassinotte <cassinotte@gmail.com>
2025-06-21 16:23:47 -0300

    ADD: Confirm on change status

--- CipaFatecJahu/wwwroot/js/Documents/history.js
+++ CipaFatecJahu/wwwroot/js/Documents/history.js
@@ -3,7 +3,13 @@
     var currentStatus = checkbox.getAttribute('data-status');
     var newStatus = checkbox.checked ? 'Ativo' : 'Inativo';
     if (currentStatus !== newStatus) {
-        var url = '/Documents/ChangeStatus?id=' + encodeURIComponent(id) + '&status=' + encodeURIComponent(newStatus);
-        window.location.href = url;
+        var mensagem = `Tem certeza que deseja mudar o status para "${newStatus}"?`;
+        if (window.confirm(mensagem)) {
+            var url = '/Documents/ChangeStatus?id=' + encodeURIComponent(id) + '&status=' + encodeURIComponent(newStatus);
+            window.location.href = url;
+        } else {
+            // Reverte o checkbox para o estado anterior
+            checkbox.checked = !checkbox.checked;
+        }
     }
 }
--- CipaFatecJahu/wwwroot/js/Secretaries/index.js
+++ CipaFatecJahu/wwwroot/js/Secretaries/index.js
@@ -3,7 +3,13 @@
     var currentStatus = checkbox.getAttribute('data-status');
     var newStatus = checkbox.checked ? 'Ativo' : 'Inativo';
     if (currentStatus !== newStatus) {
-        var url = '/Secretaries/ChangeStatus?email=' + encodeURIComponent(email) + '&status=' + encodeURIComponent(newStatus);
-        window.location.href = url;
+        var mensagem = `Tem certeza que deseja alterar o status para "${newStatus}"?`;
+        if (window.confirm(mensagem)) {
+            var url = '/Secretaries/ChangeStatus?email=' + encodeURIComponent(email) + '&status=' + encodeURIComponent(newStatus);
+            window.location.href = url;
+        } else {
+            // Reverte o checkbox para o estado anterior
+            checkbox.checked = !checkbox.checked;
+        }
     }
 }
