5060c020714f7253d9e018331ca3141b28b295e7
Evelyn Cassinotte <e.cass03@gmail.com>
2025-06-25 18:33:33 -0300

    Change: Mail Design

--- CipaFatecJahu/obj/Debug/net9.0/CipaFatecJahu.AssemblyInfo.cs
+++ CipaFatecJahu/obj/Debug/net9.0/CipaFatecJahu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CipaFatecJahu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691a3f4a1f6b7136879c6deb125d25f2a809d873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248f10a37c6602fa486029716e11bfb85106d99c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CipaFatecJahu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CipaFatecJahu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
